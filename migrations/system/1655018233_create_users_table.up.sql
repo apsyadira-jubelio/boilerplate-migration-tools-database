c02df992df450e016dbdc22357b1e15f708269a1
apsya Dira <diraapsya0@gmail.com>
2023-01-28 23:03:42 +0700

    refactor: change example migration files

--- migrations/system/1655018233_create_users_table.up.sql
+++ migrations/system/1655018233_create_users_table.up.sql
@@ -2,22 +2,16 @@
 -- +migrate StatementBegin
 
 -- USERS TABLE
-CREATE TABLE IF NOT EXISTS users (
-    user_id serial NOT NULL primary key,
+CREATE TABLE IF NOT EXISTS articles (
+    article_id serial NOT NULL primary key,
     name varchar(50) NOT NULL,
-    email varchar(50) NOT NULL ,
-    password varchar(255),
-    schema_name varchar(255) not null,
-    picture varchar(255) NOT NULL,
-    company_id BIGINT NOT NULL,
-    tenant_id varchar(255) NOT NULL,
     created_date timestamptz default now(),
     updated_date timestamptz
 );
 
-create unique index users_user_id_uindex
-    on users (user_id);
+create unique index article_article_id_uindex
+    on articles (article_id);
 -- +migrate StatementEnd
 
 -- +migrate Down
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS articles;
